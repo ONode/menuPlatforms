91449580a5555bba61cf9406683a7f60d623e031
jhes <ooxfordck@gmail.com>
2017-06-10 00:00:49 +0800

    letter
    work sheet..

--- functions/index.js
+++ functions/index.js
@@ -2,22 +2,21 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 const _ = require('lodash');
+
 const gsheet = require('google-spreadsheet-to-json');
 app.get('/api/gapi/:sheet_id', (req, res) => {
     const sheet_id = req.params.sheet_id;
     gsheet({
         spreadsheetId: sheet_id,
         // other options...
-    })
-        .then(function (result) {
-            console.log(result.length);
-            console.log(result);
-            res.json(result);
-        })
-        .catch(function (err) {
-            console.log(err.message);
-            console.log(err.stack);
-            res.json(err);
-        });
+    }).then((result) => {
+        console.log(result.length);
+        console.log(result);
+        res.json(result);
+    }).catch((err) => {
+        console.log(err.message);
+        console.log(err.stack);
+        res.json(err);
+    });
 });
 exports.app = functions.https.onRequest(app);
